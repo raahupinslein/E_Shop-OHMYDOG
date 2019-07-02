958ffc4936c8568ecb680c6b4e8b1ee562961225
carlomuhlemann <52442559+carlomuhlemann@users.noreply.github.com>
2019-07-02 15:01:44 +0200

    Add files via upload

---
+++ Project_new/Ohmydog/src/ohmydog.py
@@ -0,0 +1,12 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+from flask import Flask, render_template, request, url_for, redirect, session
+
+app = Flask(__name__)
+
+
+
+@app.route("/login", methods=[])
+if __name__ == "__main__":
+    app.run()
